68aa3666ff0986c74d8bd1b966880d43da3adaf7
Imane Bahji <imanebaha@imanes-mbp.unm.edu>
2021-08-30 12:17:25 -0600

    Added a program called hello.c that rpints Hello world

---
+++ hello.c
@@ -0,0 +1,13 @@
+// simple Hello World C file 
+
+// this is the header file for the input/output functions
+#include <iostream>
+
+using namespace std;
+
+// main function, execution of the program starts here
+int main()
+{
+	cout << "Hello World!" << endl; // prints out hello world!
+
+}
